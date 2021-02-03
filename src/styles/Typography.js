bcf7583362f695544484154661f530a4b4f16007
Nick <nickeast1998@gmail.com>
2021-02-03 15:54:37 +0000

    typography update

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -53,7 +53,7 @@ const Typography = createGlobalStyle`
 
 
     h1,h2,h3,h4,h5,h6, button {
-      font-family: Gellix-Medium, -apple-system,BlinkMacSystemFont,"Helvetica Neue",Helvetica,Arial,sans-serif;
+      font-family: -apple-system,BlinkMacSystemFont,"Helvetica Neue",Helvetica,Arial,sans-serif;
 ;
       margin: 0;
       letter-spacing: -.25px;
