26eb52a49c32aff72a9154a7093338212c42d77c
Nick <nickeast1998@gmail.com>
2021-03-01 21:39:06 +0000

    form testing 2.1

--- src/pages/form-test.js
+++ src/pages/form-test.js
@@ -3,20 +3,35 @@ import React from 'react';
 const FormTesting = () => (
   <main>
     <form
-      name="testing"
+      name="contact-testing"
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
-      action="/"
     >
-      <p>Name</p>
-      <input type="text" name="name" id="name" />
-
-      <p>email</p>
-      <input type="email" name="" id="" />
-      <p>Message</p>
-      <textarea name="message" id="message" />
-      <button type="submit">Submit</button>
+      <input type="hidden" name="form-name" value="contact" />
+      <div className="contact__wrapper__inner__form__section">
+        <input type="text" placeholder="Name" name="name" id="name" required />
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          id="email"
+          required
+        />
+      </div>
+      <input
+        type="text"
+        placeholder="Subject"
+        name="title"
+        id="title"
+        required
+      />
+      <textarea placeholder="Message" name="message" id="message" required />
+      <div className="contact__wrapper__inner__form__btn">
+        <button type="submit">
+          <span>Submit</span>
+        </button>
+      </div>
     </form>
   </main>
 );
